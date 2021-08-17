022ac569c744c6faeeb304f1507c14316afa8b4e
Ærctic <jesseberkhof95@gmail.com>
2021-08-17 23:25:20 +0200

    fixed result image

--- content.js
+++ content.js
@@ -1,10 +1,15 @@
-roddel();
-
-var observer = new MutationObserver( function(mutations) { roddel() } )
+var observer = new MutationObserver( function(mutations) { 
+    roddel();
+    results();
+})
 
 observer.observe(document.getElementById('participantList'), { attributes: true, childList: true, attributeOldValue: true })
 
 function roddel() {
+    if (document.getElementById('electionTitle').innerHTML != 'Televizier-Ster Online-Videoserie') {
+        return;
+    }
+
     document.querySelectorAll('.ring__cards .card__title').forEach(function (title) {
         title.innerHTML = "Roddelpraat"
     });
@@ -24,3 +29,7 @@ function roddel() {
     });
 }
 
+function results() {
+    const images = document.querySelectorAll('.voteresult > .ring__vote-row--image > img')
+    images[images.length - 1].src = "https://scontent-ams4-1.xx.fbcdn.net/v/t1.6435-9/182963160_165681088892504_4403830107495410915_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=973b4a&_nc_ohc=GeJ7qNcKaFcAX-Ugbnh&_nc_ht=scontent-ams4-1.xx&oh=8ec266550a6ca6eaf298b02b70a1a411&oe=6142408B"
+}
