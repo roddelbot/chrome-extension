62d5c5168d0b466a4aea1f19b388836b434f7e81
Ærctic <jesseberkhof95@gmail.com>
2021-08-20 21:01:39 +0200

    Changed only first card of category list & updated success tweet intent

--- content.js
+++ content.js
@@ -1,37 +1,33 @@
-var observer = new MutationObserver( function(mutations) { 
-    roddel();
+var observer = new MutationObserver( function() { 
+    const imgUrl = "https://scontent-ams4-1.xx.fbcdn.net/v/t1.6435-9/182963160_165681088892504_4403830107495410915_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=973b4a&_nc_ohc=GeJ7qNcKaFcAX-Ugbnh&_nc_ht=scontent-ams4-1.xx&oh=8ec266550a6ca6eaf298b02b70a1a411&oe=6142408B"
+    roddel(imgUrl);
 
     if (document.getElementById('electionTitle').innerHTML == 'Overzicht') {
-        results();
+        results(imgUrl);
     }
 })
 
 observer.observe(document.getElementById('participantList'), { attributes: true, childList: true, attributeOldValue: true })
-function roddel() {
+function roddel(imgUrl) {
     if (document.getElementById('electionTitle').innerHTML != 'Televizier-Ster Online-Videoserie') {
         return;
     }
 
-    document.querySelectorAll('.ring__cards .card__title').forEach(function (title) {
-        title.innerHTML = "Roddelpraat"
-    });
-    document.querySelectorAll('.ring__cards .card__sub-title').forEach(function (subtitle) {
-        subtitle.innerHTML = ""
-    });
-    document.querySelectorAll('.ring__cards .card__img > img').forEach(function (image) {
-        image.src = "https://scontent-ams4-1.xx.fbcdn.net/v/t1.6435-9/182963160_165681088892504_4403830107495410915_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=973b4a&_nc_ohc=GeJ7qNcKaFcAX-Ugbnh&_nc_ht=scontent-ams4-1.xx&oh=8ec266550a6ca6eaf298b02b70a1a411&oe=6142408B"
-    });
-    document.querySelectorAll('.ring__cards .clickable').forEach(function (link) {
-        link.dataset.id = "\'Roddelpraat\'"
-        link.dataset.name = "\'Roddelpraat\'"
-
-        link.onclick = function () {
-            roddel();
-        }
-    });
+    document.querySelectorAll('.ring__cards .card__title')[0].innerHTML = "RoddelPraat"
+    document.querySelectorAll('.ring__cards .card__sub-title')[0].innerHTML = "Dennis Schouten & Jan Roos"
+    
+    const image = document.querySelectorAll('.ring__cards .card__img > img')[0];
+    image.src = imgUrl
+    
+    const card = document.querySelectorAll('.ring__cards .clickable')[0];
+    card.dataset.id = "\'RoddelPraat\'"
+    card.dataset.name = "\'RoddelPraat\'"
+    card.onclick = function () {
+        roddel(imgUrl);
+    }
 }
 
-function results() {
-    const images = document.querySelectorAll('.voteresult > .ring__vote-row--image > img')
-    images[images.length - 1].src = "https://scontent-ams4-1.xx.fbcdn.net/v/t1.6435-9/182963160_165681088892504_4403830107495410915_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=973b4a&_nc_ohc=GeJ7qNcKaFcAX-Ugbnh&_nc_ht=scontent-ams4-1.xx&oh=8ec266550a6ca6eaf298b02b70a1a411&oe=6142408B"
+function results(imgUrl) {
+    document.querySelectorAll('.card__sub-title')[5].innerHTML = "Dennis Schouten & Jan Roos"
+    document.querySelectorAll('.voteresult > .ring__vote-row--image > img')[5].src = imgUrl
 }
